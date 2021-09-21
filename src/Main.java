ca2f02f8839ca3e840c878f94275078c726bdede
yangseungbin <seungbin.yang@kt.com>
2021-09-21 18:24:36 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        int[] drinks = new int[10000];
+//        int[] dp = new int[10000];
+//
+//        while(true){
+//            Scanner sc = new Scanner(System.in);
+//            int n = sc.nextInt();
+//            int b,c,ans = 0;
+//
+//            for(int i=0; i<n; i++){
+//                drinks[i] = sc.nextInt();
+//            }
+//
+//            dp[0] = drinks[0];
+//            dp[1] = drinks[0] + drinks[1];
+//            dp[2] = get_max(dp[1], drinks[0] + drinks[2]);
+//            dp[2] = get_max(dp[2], drinks[1] + drinks[2]);
+//
+//            for(int i=3; i<n; i++){
+//                dp[i] = get_max(drinks[i] + drinks[i-1] + dp[i-3], drinks[i] + dp[i-2]);
+//                dp[i] = get_max(dp[i-1], dp[i]);
+//                ans = get_max(ans, dp[i]);
+//            }
+//            System.out.println(ans);
+//            System.out.println("============");
+//        }
+//    }
+//
+//    public static int get_max(int a, int b){
+//        return a>b ? a : b;
+//    }
+//}
---
+++ src/num9461/Main.java
@@ -0,0 +1,25 @@
+//package num9461;
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        long dp[] = new long[101];
+//        int t = sc.nextInt();
+//        dp[1] = 1;
+//        dp[2] = 1;
+//        dp[3] = 1;
+//        dp[4] = 2;
+//        dp[5] = 2;
+//        for(int i=1; i<=t; i++)
+//        {
+//            int n = sc.nextInt();
+//            if(n > 5){
+//                for(int j=6; j<=n; j++) {
+//                    dp[j] = dp[j - 5] + dp[j - 1];
+//                }
+//            }
+//            System.out.println(dp[n]);
+//        }
+//    }
+//}
