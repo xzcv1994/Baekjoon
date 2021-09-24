cf91655477809ef8c7f82ce2d27320184f3340b0
yangseungbin <seungbin.yang@kt.com>
2021-09-24 16:58:38 +0900

    백준 9465번 품

--- src/num1010/Main.java
+++ src/num1010/Main.java
@@ -1,48 +1,48 @@
-package num1010;
-
-import java.util.Scanner;
-
-public class Main {
-    static int count = 0;
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        long[] arr = new long[t];
-        long[][] dp = new long[31][31];
-        long count = 0;
-
-        for(int i=0; i<t; i++){
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-
-            if(n == 1){
-                arr[i] = m;
-                continue;
-            }
-
-            for(int j=n-1; j<=m-1; j++){
-                count += solution(j, n-1, dp);
-            }
-            arr[i] = count;
-            count = 0;
-        }
-
-        for(int i=0; i<t; i++)
-            System.out.println(arr[i]);
-    }
-
-    public static long solution(int front, int rear, long[][] dp){
-       if(front == rear){
-           dp[front][rear] = 1;
-           return 1;
-       } else if(rear == 1){
-           dp[front][rear] = front;
-           return front;
-       }
-
-       if(dp[front][rear] == 0)
-            dp[front][rear] = solution(front-1, rear -1, dp) + solution(front-1, rear, dp);
-
-       return dp[front][rear];
-    }
-}
+//package num1010;
+//
+//import java.util.Scanner;
+//
+//public class Main {
+//    static int count = 0;
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int t = sc.nextInt();
+//        long[] arr = new long[t];
+//        long[][] dp = new long[31][31];
+//        long count = 0;
+//
+//        for(int i=0; i<t; i++){
+//            int n = sc.nextInt();
+//            int m = sc.nextInt();
+//
+//            if(n == 1){
+//                arr[i] = m;
+//                continue;
+//            }
+//
+//            for(int j=n-1; j<=m-1; j++){
+//                count += solution(j, n-1, dp);
+//            }
+//            arr[i] = count;
+//            count = 0;
+//        }
+//
+//        for(int i=0; i<t; i++)
+//            System.out.println(arr[i]);
+//    }
+//
+//    public static long solution(int front, int rear, long[][] dp){
+//       if(front == rear){
+//           dp[front][rear] = 1;
+//           return 1;
+//       } else if(rear == 1){
+//           dp[front][rear] = front;
+//           return front;
+//       }
+//
+//       if(dp[front][rear] == 0)
+//            dp[front][rear] = solution(front-1, rear -1, dp) + solution(front-1, rear, dp);
+//
+//       return dp[front][rear];
+//    }
+//}
---
+++ src/num9465/Main.java
@@ -0,0 +1,41 @@
+package num9465;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        int n;
+        long num1, num2;
+
+        for(int i=1; i<=t; i++){
+            n = sc.nextInt();
+            int[][] arr = new int[3][100001];
+            long[][] dp = new long[3][100001];
+
+            for(int j=1; j<=2; j++) {
+                for (int k = 1; k <= n; k++) {
+                    arr[j][k] = sc.nextInt();
+                }
+            }
+
+            dp[1][1] = arr[1][1];
+            dp[2][1] = arr[2][1];
+            dp[1][2] = arr[1][2] + dp[2][1];
+            dp[2][2] = arr[2][2] + dp[1][1];
+
+            for(int j=3; j<=n; j++){
+                dp[1][j] = arr[1][j] + getMax(dp[2][j-1], dp[2][j-2]);
+                dp[2][j] = arr[2][j] + getMax(dp[1][j-1], dp[1][j-2]);
+            }
+            System.out.println(getMax(dp[1][n], dp[2][n]));
+        }
+    }
+
+    public static long getMax(long num1, long num2){
+        return num1 >= num2 ? num1 : num2;
+    }
+}
